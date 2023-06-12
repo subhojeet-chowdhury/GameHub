ab0e5fee5e1263595b4cd378cffb25c609e659cb
Subhojeet Chowdhury <subhojeetchowdhury98@gmail.com>
2023-06-13 02:15:18 +0530

    Implemented color modes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Button, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <HStack>
       <Image src={logo} boxSize="60px"></Image>
-      <Text>GameHub</Text>
+      <Button onClick={toggleColorMode}>Toggle {colorMode === "light" ? "Dark" : "Light"}</Button>
     </HStack>
   );
 };
