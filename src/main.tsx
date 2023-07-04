13360a0e5b25d93b08ceeed88ed2a248dc38aff1
subhojeet chowdhury <subhojeetchowdhury98@gmail.com>
2023-07-04 16:43:28 +0530

    Customizing the chakra theme and refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import theme from "./theme.tsx";
+import theme from "./theme.ts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
